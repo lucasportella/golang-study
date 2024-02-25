b5d32d1c5866a65da88043eeb7a2c3f6844fe3ad
lucas <lucasportellaagu@gmail.com>
2024-02-25 12:45:15 -0300

    fix: doc-package fix missing imports and correct format

--- doc/testeDoc.go
+++ doc/testeDoc.go
@@ -1,7 +1,9 @@
-// Package doc is a template of how to export your package to godoc
+// Package doc is a template of how to export your package to godoc.
 package doc
 
-// fn to test godoc
+import "fmt"
+
+// TestDoc1 is a function to test godoc.
 func TestDoc1() {
 	fmt.Println("test godoc")
 }
